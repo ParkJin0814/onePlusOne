bb157c3292762a2cd8cb8d02ad3dbe2bbd2c3d52
ryusu12 <rsy7567@gmail.com>
2025-07-09 18:02:35 +0900

    feat: 상품 전체 조회 API에 RedisRemote Cache 적용
    기존 코드의 경우, 각 웹 서버에서 각자의 캐시를 가지고 있기 때문에, 캐시 데이터가 불일치하는 문제가 발생
    
    Redis의 Remote Cache는 외부에서 데이터를 저장하기 때문에, 여러 서버에서도 같은 캐시 데이터에 접근할 수 있음
    
    RedisConfig에 캐시 설정 추가
    - productPageRedisTemplate : RedisTemplate<String, Page<Product>> 으로 형식 지정
    - cacheManager : 캐시 유지 시간 30분으로 설정

--- src/main/java/com/example/oneplusone/global/config/RedisConfig.java
+++ src/main/java/com/example/oneplusone/global/config/RedisConfig.java
@@ -1,9 +1,15 @@
 package com.example.oneplusone.global.config;
 
+import com.example.oneplusone.domain.product.entity.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -13,5 +19,22 @@ public class RedisConfig {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Page<Product>> productPageRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Page<Product>> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)); // 캐시 시간 설정
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
