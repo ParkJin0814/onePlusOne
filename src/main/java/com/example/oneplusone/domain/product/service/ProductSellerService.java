ca6bcf0df857f94a67bc8ed90f0b4cabbee95b25
ryusu12 <rsy7567@gmail.com>
2025-07-11 10:50:19 +0900

    fix: ProductSeller 에서 토큰으로 유저 정보를 가져오기
    기존의 경우, 임의로 유저를 생성해서 진행함
    회원가입, 로그인이 반영됨에 따라 제대로 유저와 토큰을 사용할 수 있음
    
    `@AuthenticationPrincipal UserDetailsImpl userDetails` 로 토큰 정보를 가져옴
    - getUserId()로 userId 추출
    - getUserById로 user 정보 활용

--- src/main/java/com/example/oneplusone/domain/product/service/ProductSellerService.java
+++ src/main/java/com/example/oneplusone/domain/product/service/ProductSellerService.java
@@ -1,7 +1,6 @@
 package com.example.oneplusone.domain.product.service;
 
 import com.example.oneplusone.domain.auth.entity.User;
-import com.example.oneplusone.domain.auth.enums.UserRole;
 import com.example.oneplusone.domain.auth.repository.UserRepository;
 import com.example.oneplusone.domain.common.exception.BaseException;
 import com.example.oneplusone.domain.common.exception.ErrorCode;
@@ -22,22 +21,23 @@ public class ProductSellerService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
     public Product getProductById(Long productId) {
         return productRepository.findById(productId).orElseThrow(() -> new BaseException(ErrorCode.PRODUCT_NOT_FOUND));
     }
 
     @Transactional
-    public ProductSellerResponse createProduct(CreateProductRequest request) {
-        // TODO : 임시로 user 생성함 -> 토큰에서 로그인 유저 정보 가져올 예정
-        User user = new User("nickname", "login", "password", UserRole.SELLER);
-        userRepository.save(user);
-
+    public ProductSellerResponse createProduct(CreateProductRequest request, Long userId) {
         Product product = new Product(
                 request.getProductName(),
                 request.getType(),
                 request.getPrice(),
                 request.getQuantity(),
-                user
+                getUserById(userId)
         );
 
         productRepository.save(product);
@@ -46,9 +46,8 @@ public class ProductSellerService {
     }
 
     @Transactional
-    public void deleteProduct(Long id) {
-        // TODO : 임시로 USER를 찾음 -> 토큰에서 로그인 유저 정보 가져올 예정
-        User user = userRepository.findById(id).orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+    public void deleteProduct(Long id, Long userId) {
+        User user = getUserById(userId);
         Product product = getProductById(id);
 
         // 본인이 생성한 Product 만 삭제가 가능하다.
@@ -58,9 +57,8 @@ public class ProductSellerService {
     }
 
     @Transactional
-    public ProductSellerResponse updateProduct(Long id, UpdateProductRequest request) {
-        // TODO : 임시로 USER를 찾음 -> 토큰에서 로그인 유저 정보 가져올 예정
-        User user = userRepository.findById(id).orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+    public ProductSellerResponse updateProduct(Long id, UpdateProductRequest request, Long userId) {
+        User user = getUserById(userId);
         Product product = getProductById(id);
 
         // 본인이 생성한 Product 만 수정이 가능하다.
