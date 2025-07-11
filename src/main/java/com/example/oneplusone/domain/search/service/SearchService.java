6390a387367df7f731857ccda1a4ae37ad757220
ryusu12 <rsy7567@gmail.com>
2025-07-11 09:27:03 +0900

    fix: 인기 검색어 어뷰징 방지
    한 사람이 여러 요청을 통해 인기검색어 조작하는 행위 방지
    - 동일 ip로 같은 검색어에 대해 1분에 2회 요청만 인기검색어에 반영됨
    - AOP 에서 ip 가져옴
    - 검색어와 ip를 기록하는 search 캐시 추가
    - search 캐시가 3번 이상 기록되면 인기검색어 갱신을 하지 않음

--- src/main/java/com/example/oneplusone/domain/search/service/SearchService.java
+++ src/main/java/com/example/oneplusone/domain/search/service/SearchService.java
@@ -2,6 +2,7 @@ package com.example.oneplusone.domain.search.service;
 
 import com.example.oneplusone.domain.search.dto.TrendingKeywordResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
@@ -10,17 +11,28 @@ import java.time.Duration;
 import java.util.Objects;
 import java.util.Set;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    private static final String CACHE_KEY = "keyword";
+    private static final String CACHE_KEY = "keyword:";
 
-    public void saveKeyword(String keyword) {
-        redisTemplate.opsForZSet().incrementScore(CACHE_KEY, keyword, 1);
-        redisTemplate.expire(CACHE_KEY, Duration.ofMinutes(10));
+    public void saveKeyword(String keyword, String ip) {
+        String keyAndIp = "search:" + keyword + "_" + ip;
+        Long count = redisTemplate.opsForValue().increment(keyAndIp, 1); // 요청별 카운트
+        if (count != null && count == 1) {
+            // 요청이 들어온 기준으로 1분 제한
+            redisTemplate.expire(keyAndIp, Duration.ofMinutes(1));
+        }
+        if (count != null && count <= 2) {
+            // 같은 ip의 요청이 1분동안 2번을 넘어가면 인기검색어 카운트 안함
+            redisTemplate.opsForZSet().incrementScore(CACHE_KEY, keyword, 1); // 인기 검색어 카운트
+            redisTemplate.expire(CACHE_KEY, Duration.ofMinutes(10));
+            log.info("검색 키워드 저장: {}", keyword);
+        }
     }
 
     public TrendingKeywordResponse getTrendingKeywords(int limit) {
--- src/main/java/com/example/oneplusone/global/aop/SearchKeywordAspect.java
+++ src/main/java/com/example/oneplusone/global/aop/SearchKeywordAspect.java
@@ -1,17 +1,18 @@
 package com.example.oneplusone.global.aop;
 
 import com.example.oneplusone.domain.search.service.SearchService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class SearchKeywordAspect {
     private final SearchService searchService;
 
@@ -19,16 +20,27 @@ public class SearchKeywordAspect {
     // 정상종료되었을때만 실행
     @AfterReturning(
             pointcut = "execution(* com.example.oneplusone.domain.product.service.ProductService.productsPage(..))"
-            + " || execution(* com.example.oneplusone.domain.product.controller.ProductController.productsPageV2(..))"
+                    + " || execution(* com.example.oneplusone.domain.product.controller.ProductController.productsPageV2(..))"
     )
     public void logSearchKeyword(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
 
         if (args.length >= 1 && args[0] instanceof String searchKeyword) {
             if (!searchKeyword.isBlank()) {
-                searchService.saveKeyword(searchKeyword);
-                log.info("검색 키워드 저장: {}", searchKeyword);
+                searchService.saveKeyword(searchKeyword, getClientIp());
             }
         }
+    }
+
+    private String getClientIp() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) return "unknown";
+        HttpServletRequest request = attributes.getRequest();
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
