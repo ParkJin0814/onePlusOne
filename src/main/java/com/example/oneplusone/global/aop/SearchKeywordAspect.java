19758a082f6d1919c9ba9ede666c60a167f20741
roog23 <roog2320@gmail.com>
2025-07-14 11:25:26 +0900

    refactor : 잘못 지정한 부분 수정
    aop에서 redisLockRepository 삭제

--- src/main/java/com/example/oneplusone/global/aop/SearchKeywordAspect.java
+++ src/main/java/com/example/oneplusone/global/aop/SearchKeywordAspect.java
@@ -1,6 +1,5 @@
 package com.example.oneplusone.global.aop;
 
-import com.example.oneplusone.domain.common.redis.RedisLockRepository;
 import com.example.oneplusone.domain.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SearchKeywordAspect {
     private final SearchService searchService;
-    private final RedisLockRepository redisLockRepository;
 
 //    // 정상종료되었을때만 실행
 //    @AfterReturning(
